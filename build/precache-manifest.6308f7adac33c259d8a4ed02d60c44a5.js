c4b0db05e8b147c7937cb1b736d500cadb8730b5
jhw4319 <46766887+jhw4319@users.noreply.github.com>
2020-01-25 21:58:32 +0900

    Project: react-app
    first branch

---
+++ build/precache-manifest.6308f7adac33c259d8a4ed02d60c44a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9eeccd672b36d738718d7221228c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa0d44a4374abb5cc889",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "252f03f843837159fe86",
+    "url": "/static/js/2.9b5e4655.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9b5e4655.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa0d44a4374abb5cc889",
+    "url": "/static/js/main.da7a95c4.chunk.js"
+  },
+  {
+    "revision": "743eb91adfc0d0e6386a",
+    "url": "/static/js/runtime-main.8616811a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
