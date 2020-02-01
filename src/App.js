8557e7b1ecfb0248ca15cd16c45bf661eca952ea
jhw4319 <46766887+jhw4319@users.noreply.github.com>
2020-02-01 22:29:17 +0900

    event
    event設定
    ・state,props event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import TOC from "./components/TOC"
 import Content from "./components/Content"
 import Subject from "./components/Subject"
@@ -9,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,15 +19,30 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null; 
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+          }}>{this.props.title}</a></h1>
+          {this.props.sub}
+        </header>
         <Subject title="React" sub="For UI"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
